d5e69f70056f141d15c7eb5ec546d8ea4fae4c08
CodingMirec <m.sykora01@gmail.com>
2020-10-18 20:20:39 +0200

    handles, const

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStateValue } from "./StateProvider";
 import { Link } from "react-router-dom";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+
 import CheckoutProduct from "./CheckoutProduct";
 
 import "./Payment.css";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = (e) => {};
+
+  const handleChange = (e) => {
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -50,7 +66,27 @@ function Payment() {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details">{/* Stripe magic */}</div>
+          <div className="payment__details">
+            {/* Stripe magic */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
