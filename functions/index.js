d573ef684d0b6a9182f1cadab685225e0178e4e1
CodingMirec <m.sykora01@gmail.com>
2020-10-19 19:29:38 +0200

    added express, cors, stripe // created main part of BE

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Hdfv3CvqKP3FC1OIEiIPt1WONhpJcPhgsNQSEmdP7IxnO0KlRuA1LMEoDpRcqbXR1jgFO5XeOADCN1BdsA9wHmf00RvZFHxQY"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//API
+
+//App config
+const app = express();
+
+//Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//API routes
+app.get("/", (request, response) => res.status(200).send("hello world"));
+
+//Listen Command
+exports.api = functions.https.onRequest(app);
