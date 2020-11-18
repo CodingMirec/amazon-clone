7093fb2c917ae7e088d79192e55967b6eeec909b
CodingMirec <m.sykora01@gmail.com>
2020-11-18 22:18:11 +0100

    payment styling

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/mirec--clone/us-central1/api", //THE API URL
+  baseURL: "https://us-central1-mirec--clone.cloudfunctions.net/api",
+  // "http://localhost:5001/mirec--clone/us-central1/api", //THE API URL
 });
 
 export default instance;
